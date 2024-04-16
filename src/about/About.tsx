c2d9f7a332571ddc5e3e9f1203c7c1ac1c1beb6b
eliecer garcia <absalonsubzero@gmail.com>
2024-04-15 19:05:23 -0600

    correccion de doble navbar

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -7,7 +7,7 @@ export const About = () => {
       className="min-h-[90vh] p-5 bg-primary flex gap-8 flex-col lg:flex-row-reverse"
     >
       <img
-        src="https://s3-alpha-sig.figma.com/img/566c/53c0/a7bd87bd0aaf46ca578e5430808811aa?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Tb-LrSCg4sxzGJk1bltXXeRUlaKkRiOXsU2E8Cw692BvT9pJFGWysiDqXeYMFIb8WIgNJNHOjcwrUHQ95IeRzwqfYSM4h8KQeN8b8eTKjxzL3rhKOQJA9EWvJlNJ6FGxa8SSY~bAsZtSGXsXWe0mcI9PAXqRNKQrUhfhBUPeNUdCO4brJAC58rmcr~Ah6TcQCnbyyHqmIoohtFpKTJwBgYwqWUa3ndNe7naGq~y7HtCCR2WvNKfbeTD4Vxa4CDhql306s5MdB-WG1-0zRtXv2~7pnCLuB4i1jqMgs625RbJvfZGfnYPDBrC741MK4MtR2hPAVprUSoTlOUUG-CBrHw__"
+        src="https://s3-alpha-sig.figma.com/img/566c/53c0/a7bd87bd0aaf46ca578e5430808811aa?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hZFxlUtiNgRWketBznDZnH50SughTaImyatT6di6n0pqBzfh24WArRLqjad08iexWxKhAljWtDpnNgPH6IgPe2HO2atg1XpCZRiL0Dag5upog2-h4fLwnEjrGBYUf1VUwU9MWEzllEgDJ8PslKF2e1f0h-SbqzDAJura457VXgRwuQ69ERA2LMk8qlaxMd-NAdXvbwK0LMQQMAD~yDdsKBJgKVzVIbTHaKBLjI6ntSeAp1r~wPIral9X4DfY5z6yxeLwUTT~KCuTTEBSO5UU8UkwFE0QETXRkAxNtKbP4zC7hL73eR5vlf1ICAbZbs7WJYl~MgIOGPMOjjFPRx3xBA__"
         alt="aboutImage"
         className="object-cover w-full lg:w-1/2"
       />
