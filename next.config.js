e21854218b2d129963336d65686786f111c39385
Eliecer Garcia <absalonsubzero@gmail.com>
2024-04-27 00:15:35 -0600

    modificacion next config

--- next.config.js
+++ next.config.js
@@ -5,17 +5,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // const baseUrl = '/flatwhite';
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  trailingSlash: true,
-  // basePath: baseUrl,
-  // env: {
-  //   baseUrl,
-  // },
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  output: "export", 
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-});
+};
+
+module.exports = nextConfig;
