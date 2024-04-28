db8a5b4da9267a485140bfdd89da1c1d28114862
Celso Cardenas <cardenascelso9@gmail.com>
2024-04-27 22:48:08 -0600

    change index to static and not ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-import { GetStaticProps } from 'next';
-
-import { IBlogGalleryProps } from '../blog/BlogGallery';
 import { Contact } from '../contact/Contact';
 import { Hero } from '../hero/Hero';
 import { Meta } from '../layout/Meta';
-import { IPaginationProps } from '../pagination/Pagination';
 import { Main } from '../templates/Main';
 import { AppConfig } from '../utils/AppConfig';
-import { getAllPosts } from '../utils/Content';
 import { About } from '../about/About';
+
+//import for posts generations
+// import { GetStaticProps } from 'next';
+
+// import { IPaginationProps } from '../pagination/Pagination';
+// import { getAllPosts } from '../utils/Content';
+// import { IBlogGalleryProps } from '../blog/BlogGallery';
 
 
 const Index = () => (
@@ -28,20 +30,20 @@ const Index = () => (
   </Main>
 );
 
-export const getStaticProps: GetStaticProps<IBlogGalleryProps> = async () => {
-  const posts = getAllPosts(['title', 'date', 'slug']);
-  const pagination: IPaginationProps = {};
+// export const getStaticProps: GetStaticProps<IBlogGalleryProps> = async () => {
+//   const posts = getAllPosts(['title', 'date', 'slug']);
+//   const pagination: IPaginationProps = {};
 
-  if (posts.length > AppConfig.pagination_size) {
-    pagination.next = '/page2';
-  }
+//   if (posts.length > AppConfig.pagination_size) {
+//     pagination.next = '/page2';
+//   }
 
-  return {
-    props: {
-      posts: posts.slice(0, AppConfig.pagination_size),
-      pagination,
-    },
-  };
-};
+//   return {
+//     props: {
+//       posts: posts.slice(0, AppConfig.pagination_size),
+//       pagination,
+//     },
+//   };
+// };
 
 export default Index;
